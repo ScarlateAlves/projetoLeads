f55411f88a0023fc18c75eaab2ebe669114aff95
Scarlate <scarlate100@gmail.com>
2021-04-07 17:24:57 -0300

    modificado pagina principal

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,68 @@
 <template>
   <div class="leads">
     <Logo />
-    <!-- <Logo dark-background /> -->
-    <h1 class="leads__title">Leads</h1>
+    <div class="leads__container">
+      <h1 class="leads__title">Leads</h1>
+      <div class="leads__div">
+        <search-input tipo="text" name="Search name" v-model="filtro" />
+        <search-dropdown
+          v-model="selected"
+          :options="categories"
+        ></search-dropdown>
+      </div>
+    </div>
+    <base-table :registros="filtre" />
   </div>
 </template>
-
 <script>
-export default {}
+import des from '~/utils/index'
+import SearchInput from '~/components/shared/search/SearchInput.vue'
+import SearchDropdown from '~/components/shared/search/SearchDropdown.vue'
+import BaseTable from '~/components/shared/table/BaseTable.vue'
+export default {
+  components: {
+    'search-input': SearchInput,
+    'search-dropdown': SearchDropdown,
+    'base-table': BaseTable,
+  },
+  data() {
+    return {
+      filtro: '',
+      leads: [],
+      selected: '',
+    }
+  },
+  computed: {
+    categories() {
+      return des.destructing(this.leads)
+    },
+    filtre() {
+      return des.filtre(this.filtro, this.selected, this.leads)
+    },
+  },
+  async asyncData(ctx) {
+    return {
+      leads: await ctx.app.$repository.index(),
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped="true">
 .leads {
-  &__title {
-    margin: 1.4rem 0;
+  font-family: 'Work Sans', sans-serif;
+
+  &__container {
+    margin-top: 1.4rem;
     padding: 1.4rem 0;
+    display: flex;
+    justify-content: space-between;
     border-top: $border-color 1px solid;
+  }
+  &__div {
+    display: flex;
+    width: 50%;
+    justify-content: space-between;
   }
 }
 </style>
