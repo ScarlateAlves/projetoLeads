6a0c3816b1dfbd9c1bee72670c0c4765748e42df
Scarlate <scarlate100@gmail.com>
2021-04-07 17:21:39 -0300

    criado componete de dropdown

---
+++ components/shared/search/SearchDropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <select @input="$emit('input', $event.target.value)">
+    <option value="">Categories</option>
+    <option v-for="text in options" :key="text.id">
+      {{ text }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  props: ['options'],
+}
+</script>
+<style scoped lang="scss">
+select {
+  width: 40%;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 4px;
+  border: $border-color 1.5px solid;
+  border-radius: 10px;
+  color: rgb(116, 129, 138);
+  font-weight: 500;
+  padding: 0.5rem;
+}
+</style>
