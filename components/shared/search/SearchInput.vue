a59f6e954385185e8ab65f547be7931faf556457
Scarlate <scarlate100@gmail.com>
2021-04-07 17:20:43 -0300

    criado componete de input

---
+++ components/shared/search/SearchInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <label>
+      {{ label }}
+    </label>
+    <input
+      :type="tipo"
+      :placeholder="name"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    tipo: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+div {
+  width: 50%;
+}
+input {
+  height: 47px;
+  font-size: 16px;
+  width: 100%;
+  border: $border-color 1.5px solid;
+  border-radius: 10px;
+  padding-left: 2rem;
+  background-image: url('~/img/pesquisa.png');
+  background-repeat: no-repeat;
+  background-position: 10px 15px;
+}
+</style>
